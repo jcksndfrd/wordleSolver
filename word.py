f37aad0f00f757984281aea6ce5dfe9258e6cc7a
Jack Sandford <jacksandford5757@gmail.com>
2022-02-10 04:09:09 +1300

    improved algorithm, updated word list

--- word.py
+++
@@ -1,7 +0,0 @@
-def messShitUp(thisIsAList):
-    thisIsAList = "ajhdwgwajd"
-    return
-
-thisIsAnotherList = "[1, 2, 3, 4, 5]"
-messShitUp(thisIsAnotherList)
-print(thisIsAnotherList)
--- wordle.py
+++ wordle.py
@@ -1,59 +1,67 @@
-def getLetters(words):
-    letters = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
+## change from letters to letter placement for rating
 
+def rateLetters(words, letters):
+    scores = dict.fromkeys(letters, 0)
     for word in words:
         for letter in word:
-            letters[letter] += 1
+            if letter in letters:
+                scores[letter] += 1
 
-    return [item[0] for item in sorted(letters.items(), key=lambda item:item[1])]
+    return scores
 
 def rateWord(word, letters):
     score = 0
-    for i, letter in enumerate(letters):
-        for l in word:
-            if letter == l:
-                score += i
+    for letter, value in letters.items():
+        if letter in word:
+            score += value
     
-    return (word, score)
+    return score
 
 
-def getSortedWords(wordList):
-    letters = getLetters(wordList)
-    return sorted([rateWord(word, letters) for word in wordList], key=lambda word: word[1], reverse=True)
+def getSortedWords(words, letters):
+    letters = rateLetters(words, letters)
+    return sorted(words, key=lambda word: rateWord(word, letters), reverse=True)
 
-guess = input("Guess: ")
-words = []
+def getBestWord(words, letters):
+    letters = rateLetters(words, letters)
+    return max(words, key=lambda word: rateWord(word, letters))
 
 with open("words.txt") as f:
     words = [word.strip() for word in f.readlines()]
 
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-while True:
-    for i in range(5):
-        status = int(input("Letter " + str(i) + ": "))
+for _ in range(6):
+    guess = getBestWord(words, letters)
+    print("Guess: " + guess)
 
-        words_updated = []
-
-        if status == 0:
-            for word in words:
-                if guess[i] not in word:
-                    words_updated.append(word)
-
-        elif status == 1:
-            for word in words:
-                if word[i] == guess[i]:
-                    words_updated.append(word)
-
-        elif status == 2:
-            for word in words:
-                if word[i] != guess[i] and guess[i] in word:
-                    words_updated.append(word)
-
-        words = words_updated
+    status = [int(i) for i in input("Staus: ")]
     
-    print(getSortedWords(words))
-
-    if input("Again? ").lower() != "y":
+    if status == [1, 1, 1, 1, 1]:
+        print("Yay!")
         break
 
-    guess = input("Guess: ")
+    for i, s in enumerate(status):
+        if s == 0:
+            updated_words = []
+
+            for word in words:
+
+                for j, letter in enumerate(word):
+                    if status[j] == 2 and letter == guess[i]:
+                        updated_words.append(word)
+
+                if guess[i] not in word:
+                    updated_words.append(word)
+
+            words = updated_words
+
+        elif s == 1:
+            words = [word for word in words if word[i] == guess[i]]
+            try:
+                letters.remove(guess[i])
+            except ValueError:
+                pass
+
+        elif s == 2:
+            words = [word for word in words if word[i] != guess[i] and guess[i] in word]
